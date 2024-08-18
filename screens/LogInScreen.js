b3ad9c92df075900f8692a4b20813d412a91402b
Rogelio A <tlayoatllink@hotmail.com>
2024-08-18 14:30:30 -0600

    60

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -77,7 +77,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     title: {
-        fontSize: 70,
+        fontSize: 60,
         color: '#34434D',
         fontWeight: 'bold',
         alignItems: 'center'
