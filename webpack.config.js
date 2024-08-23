e3e2875c1b697c99bbc60b999e0816280d10e8dc
Rogelio A <tlayoatllink@hotmail.com>
2024-08-23 16:36:28 -0600

    ////

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // Add this section to include the crypto polyfill
+  // Add polyfills for crypto and stream
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    crypto: require.resolve('crypto-browserify')
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify')
   };
 
   return config;
